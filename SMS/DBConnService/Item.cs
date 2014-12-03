15e1f4e6cd1c9b7f5dc3d57e81d3323ff2c30318
Dan Willingham <willingham.dan@gmail.com>
2014-12-03 11:11:00 -0800

    redoing everything because i guess it didnt transfer all of the files?

---
+++ SMS/DBConnService/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DBConnService
+{
+    public class Item
+    {
+        public List<string> subItems;
+        public Item()
+        {
+            subItems = new List<string>();
+        }
+
+        
+    }
+}
